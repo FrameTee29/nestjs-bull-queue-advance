f236e5ba844e387cc284b024f90a6acf4a582e37
Teeraphat Sittinantakul <dev.teeraphat@gmail.com>
2023-07-12 11:17:09 +0700

    ✨ feat | initial repo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+
 import { AppController } from './app.controller';
 
+import { DepositModule } from './deposit/deposit.module';
+import { WithdrawModule } from './withdraw/withdraw.module';
+import { AccountModule } from './account/account.module';
+
 @Module({
-  imports: [],
+  imports: [DepositModule, WithdrawModule, AccountModule],
   controllers: [AppController],
   providers: [],
 })
